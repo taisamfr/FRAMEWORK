731cc8bab08f5c457b4e443ce727ed7a85d8a16c
taisinha <taisa.reis1a@gmail.com>
2025-09-22 14:57:31 -0300

    hello

---
+++ petshop/src/test/java/com/devsenai2A/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenai2A.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
